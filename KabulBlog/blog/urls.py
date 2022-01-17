818c34dd15039516dfeee51427ee4d4560334fec
Abdur Rahman <abdurahman112299@gmail.com>
2022-01-17 10:28:17 +0430

    Updating

--- KabulBlog/blog/urls.py
+++ KabulBlog/blog/urls.py
@@ -1,10 +1,15 @@
+from unicodedata import name
 from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
-from .views import home,post,category
+from .views import home,post,category,user_login,user_register,user_logout
+app_name = 'blog'
 urlpatterns = [
-   path('home/',home),
-   path('blog/<slug:post_url>',post),
-   path('category/<slug:url>',category)
+   path('',home,name='home'),
+   path('blog/<slug:post_url>',post,name="post"),
+   path('category/<slug:url>',category, name="category"),
+   path('register',user_register,name='register'),
+   path('login',user_login,name='login'),
+    path('logout/',user_logout, name="logout"),
 ]+static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
--- KabulBlog/blog/views.py
+++ KabulBlog/blog/views.py
@@ -1,7 +1,10 @@
 
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from blog.models import Post,Category
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
 # Create your views here.
 
 def home(request):
@@ -36,4 +39,51 @@ def category(request,url):
         'posts': posts
     }
     return render(request,'category.html',context)
-# 3:4
+
+def user_register(request):
+    if request.method == 'POST':
+        firstname = request.POST.get('firstname')
+        lastname = request.POST.get('lastname')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+        if password1 != password2:
+            messages.warning(request,"Password does not match")
+            return redirect('blog:register')
+        elif User.objects.filter(username=username).exists():
+            messages.warning(request,"Username already taken")
+            return redirect('blog:register')
+        elif User.objects.filter(email=email).exists():
+            messages.warning(request,"Email already taken")
+            return redirect('blog:register')
+        else:
+            user = User.objects.create_user(
+            first_name = firstname,
+            last_name = lastname,
+            username= username,
+            email = email,
+            password = password1,
+            )
+            user.save()
+            messages.success(request,'User has been registered successully')
+            return redirect('blog:login')
+    return render(request,'register.html')
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request,username = username,password = password)
+        # the fist username and password comming from the database
+        if user is not None:
+            login(request,user)
+            return redirect('blog:home')
+        else:
+            messages.warning(request,"Invalid username or password")
+            return redirect('blog:login')
+    return render(request,'login.html')
+
+def user_logout(request):
+    logout(request)
+    return redirect("blog:home")
+
